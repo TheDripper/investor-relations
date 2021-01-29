3b9e471d41ccbbeef15a3ba6d00bec4b47dec919
Dead Customer <deadcustomer@Deads-MacBook-Air.local>
2021-01-29 01:06:34 -0800

    tabs

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,59 +4,123 @@
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
+<style lang="scss">
 *,
 *::before,
 *::after {
   box-sizing: border-box;
   margin: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+.icons {
+  img {
+    max-width: 100px;
+  }
+}
+@font-face {
+  font-family: "heading";
+  src: url("/head-book.ttf");
+}
+@font-face {
+  font-family: "body-bold";
+  src: url("/body-bold.ttf");
+}
+@font-face {
+  font-family: "body-heavy";
+  src: url("/body-heavy.ttf");
+}
+@font-face {
+  font-family: "body-light";
+  src: url("/body-light.ttf");
+}
+@font-face {
+  font-family: "body-reg";
+  src: url("/body-reg.ttf");
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "heading";
+  color: #252525;
+}
+.button {
+  font-family: "body-bold";
+  font-size: 16px;
+  color: #252525;
+  text-transform: uppercase;
   text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
+  @apply p-4 pr-12 border-2 rounded relative;
+  border-color: #252525;
   display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+  min-width: 315px;
+  &:after {
+    content: url("/TriangleDark.svg");
+    @apply absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+.hero {
+  background-image: url("/Benefits.jpg");
+  background-size: cover;
+  background-position: center;
+  height: 600px;
+  h1 {
+    color: white;
+  }
+}
+.bg-grey {
+  background: linear-gradient(to right, #191919, #282828);
+  p {
+    color: white;
+  }
+  .button {
+    color: #a6a6a6;
+    border-color: #a6a6a6;
+    min-width: none;
+    &:after {
+      content: url("/Triangle.svg");
+    }
+  }
+}
+h1 {
+  font-size: 80px;
+  line-height: 1;
+}
+h3 {
+  color: #948477;
+  font-size: 20px;
+  font-family: "body-bold";
+}
+p {
+  font-family: "body-reg";
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.border-grey {
+  border-color: #444444 !important;
+}
+.border-light-grey {
+  border-color: #a6a6a6 !important;
+}
+h2 {
+  font-size: 50px;
+}
+p,
+li,
+a {
+  font-family: "body-reg";
+  color: #494949;
+}
+a {
+  text-decoration: underline;
+}
+.text-green {
+  color: #376205 !important;
+}
+.font-bold {
+  font-family: "body-heavy" !important;
 }
 </style>
---
+++ pages/stock-information.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="stock-information">
+    <div class="flex">
+      <div
+        v-on:click="thisTab"
+        data-tab="0"
+        class="tab w-1/5 mx-2 py-2 border-2 border-light-grey"
+      >
+        Quote & Chart
+      </div>
+      <div
+        v-on:click="thisTab"
+        data-tab="1"
+        class="tab w-1/5 mx-2 py-2 border-2 border-light-grey"
+      >
+        Tab 2
+      </div>
+      <div
+        v-on:click="thisTab"
+        data-tab="2"
+        class="tab w-1/5 mx-2 py-2 border-2 border-light-grey"
+      >
+        Tab 3
+      </div>
+      <div
+        v-on:click="thisTab"
+        data-tab="3"
+        class="tab w-1/5 mx-2 py-2 border-2 border-light-grey"
+      >
+        Tab 4
+      </div>
+      <div
+        v-on:click="thisTab"
+        data-tab="4"
+        class="tab w-1/5 mx-2 py-2 border-2 border-light-grey"
+      >
+        Tab 5
+      </div>
+    </div>
+    <div class="tab-content">
+      <div id="tab-0" v-if="currentTab == 0">
+        <h1>tab 1</h1>
+      </div>
+      <div id="tab-1" v-if="currentTab == 1">
+        <h1>tab 2</h1>
+      </div>
+      <div id="tab-2" v-if="currentTab == 2">
+        <h1>tab 3</h1>
+      </div>
+      <div id="tab-3" v-if="currentTab == 3">
+        <h1>tab 4</h1>
+      </div>
+      <div id="tab-4" v-if="currentTab == 4">
+        <h1>tab 5</h1>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.tab {
+  @apply flex items-center justify-center rounded;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      currentTab: 0
+    };
+  },
+  methods: {
+    thisTab(e) {
+      console.log(e.target.dataset.tab);
+      this.currentTab = e.target.dataset.tab;
+    }
+  }
+};
+</script>
